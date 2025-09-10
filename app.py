312df32f8e210cccab701892b44c50b9628859e9
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2025-09-11 00:57:18 +0800

    Use redis to enhance multithreading capabilities

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+import os
 import random
 import string
-import time
 
+import redis
 from authlib.integrations.flask_oauth2 import current_token
 from flask import Flask, g, request, jsonify
 from flask_oidc import OpenIDConnect
 from werkzeug.middleware.proxy_fix import ProxyFix
-from flask_scheduler import Scheduler
 
-import models
+from models import MembershipToken
 
 app = Flask(__name__)
 app.config['OIDC_RESOURCE_SERVER_ONLY'] = True
@@ -17,18 +17,8 @@ app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 oidc = OpenIDConnect(app)
-scheduler = Scheduler(app)
-membership_tokens = {}
-
-
-@scheduler.runner(interval=5)
-def purge_expired_token():
-    try:
-        for k, v in membership_tokens.items():
-            if v.is_expired():
-                membership_tokens.pop(k)
-    except Exception as e:
-        app.logger.exception(e)
+redis_client = redis.Redis(host=os.environ.get('REDIS_HOST', 'localhost'),
+                           port=int(os.environ.get('REDIS_PORT', '6379')), db=0)
 
 
 @app.route('/')
@@ -53,15 +43,21 @@ def get_code():
     current_token["access_token"] = auth.split(None, 1)[1]
     profile = g._oidc_auth.userinfo(token=current_token)
     code = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(20))
-    membership_tokens[code] = models.MembershipToken(profile['name'], profile['email'], time.time() + 300)
+    token = MembershipToken(name=profile['name'], email=profile['email'], username=profile['preferred_username'])
+    redis_client.set(code, token.serialize(), ex=300)
     return jsonify({"code": code})
+
 
 @app.route('/validate_code/<code>')
 def validate_code(code):
-    token = membership_tokens.get(code)
-    if token and not token.is_expired():
-        return jsonify({"name": token.name, "email": token.email, "valid": True})
-    return jsonify({"error": "Invalid or expired token.", "valid": False}), 400
+    try:
+        token = MembershipToken.deserialize(redis_client.get(code))
+        if token:
+            return jsonify({"token": dict(token), "valid": True})
+        return jsonify({"error": "Invalid or expired token.", "valid": False}), 400
+    except Exception as e:
+        return jsonify({"error": "Token deserialization error.", "valid": False}), 400
+
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,15 +1,20 @@
-import time
-
-
 class MembershipToken:
     name = None
     email = None
-    expiry = None
+    username = None
 
-    def __init__(self, name, email, expiry):
+    def __init__(self, name, email, username):
         self.name = name
         self.email = email
-        self.expiry = expiry
+        self.username = username
 
-    def is_expired(self):
-        return time.time() >= self.expiry
+    def serialize(self):
+        return f"{self.name}|##|{self.email}|##|{self.username}"
+
+    @classmethod
+    def deserialize(cls, data):
+        try:
+            name, email, username = data.split("|##|")
+            return cls(name, email, username)
+        except ValueError:
+            return None
